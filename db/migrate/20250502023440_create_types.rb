aa414983dab8f8c06785e0d3bae729f2d1b8f5b2
Joao <jfjoao12@gmail.com>
2025-05-01 21:36:50 -0500

    Added type model

---
+++ db/migrate/20250502023440_create_types.rb
@@ -0,0 +1,8 @@
+class CreateTypes < ActiveRecord::Migration[8.0]
+  def change
+    create_table :types do |t|
+      t.string :type
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_02_022647) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_02_023440) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -105,6 +105,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_02_022647) do
     t.datetime "updated_at", null: false
     t.index ["brand_id"], name: "index_phones_on_brand_id"
     t.index ["external_id"], name: "index_phones_on_external_id", unique: true
+  end
+
+  create_table "types", force: :cascade do |t|
+    t.string "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "phones", "brands"
