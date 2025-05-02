150289d582e85bf3671b825fc63dfcfdc444b15d
Joao <jfjoao12@gmail.com>
2025-05-02 06:10:10 -0500

    Full functionality of cart added

--- config/routes.rb
+++ config/routes.rb
@@ -5,17 +5,16 @@ Rails.application.routes.draw do
   get "device/:id", to: "device#show", as: :device
 
   resources :devices, only: [ :show ]
+  resource :cart, only: [ :show ]
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  # Defines the root path route ("/")
+  # add a device to cart
+  post   "/cart/:id",   to: "carts#create",  as: :add_to_cart
+
+  # remove a device from cart
+  delete "/cart/:id",   to: "carts#destroy", as: :remove_from_cart
+
   root "home#index"
 end
