0034ee51e35be86dd45f51fd0d21c0e60995d773
Joao <jfjoao12@gmail.com>
2025-05-01 21:51:46 -0500

    Added devices to active admin

---
+++ app/admin/devices.rb
@@ -0,0 +1,52 @@
+ActiveAdmin.register Device do
+  permit_params :phone_id, :storage, :color, :price, :imei
+
+  # Filters
+  filter :phone, collection: proc { Phone.order(:name) }
+  filter :storage
+  filter :color
+  filter :price
+
+  # Index
+  index do
+    selectable_column
+    id_column
+    column :phone
+    column :storage
+    column :color
+    column :price
+    column :imei
+    actions
+  end
+
+  # Show
+  show do
+    attributes_table do
+      row :phone
+      row :storage
+      row :color
+      row :price
+      row :imei
+      row :created_at
+      row :updated_at
+    end
+  end
+
+  # Form
+  form do |f|
+    f.semantic_errors *f.object.errors.keys
+
+    f.inputs "Device Details" do
+      f.input :phone,
+              as: :select,
+              collection: Phone.order(:name),
+              prompt: "Select a phone…"
+      f.input :storage
+      f.input :color
+      f.input :price
+      f.input :imei
+    end
+
+    f.actions
+  end
+end
