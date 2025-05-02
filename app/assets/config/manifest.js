335f1fe20e61338c21078c3e3be6ea416bff7cd7
Joao <jfjoao12@gmail.com>
2025-05-02 02:28:41 -0500

    Index page working

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,9 @@
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
 //= link activeadmin_addons/all.css
+//= link application.js
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+  //= link controllers/index.js
+
+
